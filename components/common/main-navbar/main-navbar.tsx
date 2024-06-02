7d3e803bee14ba4d8621b8f648dc120f17935291
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-06-02 16:50:28 +0530

    feat: :sparkles: add toggle navbar state

--- components/common/main-navbar/main-navbar.tsx
+++ components/common/main-navbar/main-navbar.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import { Menu } from "lucide-react";
+import { useMainNavbar } from "@/components/common/main-navbar/hook";
 
 const MainNavbar = () => {
+	const { toggleSidebarState } = useMainNavbar();
+
 	return (
 		<nav className="w-full fixed top-0 left-0 flex items-center px-5 py-4 bg-background dark:bg-dark-background">
 			<button
 				className="flex items-center justify-center text-foreground dark:text-dark-foreground"
 				aria-label="toggle sidebar"
+				onClick={toggleSidebarState}
 			>
 				<Menu size={23} />
 			</button>
