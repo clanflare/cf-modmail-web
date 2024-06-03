99c34a09077401ef7422a0dd17bbd857c130ca00
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-06-03 19:56:35 +0530

    feat: :sparkles: add solid btn

--- components/common/main-sidebar/main-sidebar.tsx
+++ components/common/main-sidebar/main-sidebar.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useMainSidebar } from "./hook";
+import { SolidBtn } from "@/components/elements";
+import { Power } from "lucide-react";
 
 const MainSidebar = () => {
 	const { isSidebarOpen } = useMainSidebar();
@@ -8,8 +10,24 @@ const MainSidebar = () => {
 	return (
 		<>
 			{isSidebarOpen && (
-				<div className="w-64 h-screen fixed top-0 left-0 z-10 bg-card dark:bg-dark-card border-r border-dashed border-foreground/20 dark:border-dark-foreground/20 flex flex-col">
+				<div className="w-64 h-screen fixed top-0 left-0 z-10 bg-card dark:bg-dark-card border-r border-dashed border-foreground/20 dark:border-dark-foreground/20 flex flex-col px-4 py-2">
 					<div className="w-full flex-1 flex flex-col"></div>
+
+					<SolidBtn
+						title="Logout"
+						className="mb-2"
+						titleClassName="text-destructive dark:text-destructive"
+						LeftIcon={Power}
+						leftIconClassName="text-destructive dark:text-destructive mr-2"
+					/>
+
+					<SolidBtn
+						title="Logout"
+						className="bg-destructive/20 dark:bg-destructive/20 hover:bg-destructive/30 dark:hover:dark:bg-destructive/30 border border-destructive"
+						titleClassName="text-destructive dark:text-destructive"
+						LeftIcon={Power}
+						leftIconClassName="text-destructive dark:text-destructive mr-2"
+					/>
 				</div>
 			)}
 		</>
---
+++ components/elements/solid-btn/solid-btn.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { LucideIcon } from "lucide-react";
+import { IconType } from "react-icons";
+import { twMerge } from "tailwind-merge";
+
+interface Props extends React.ComponentProps<"button"> {
+	className?: string;
+	onClick?: (e?: React.MouseEvent<HTMLButtonElement> | undefined) => void;
+	title: string;
+	titleClassName?: string;
+	LeftIcon?: LucideIcon | IconType;
+	leftIconSize?: number;
+	leftIconClassName?: string;
+	RightIcon?: LucideIcon | IconType;
+	rightIconSize?: number;
+	rightIconClassName?: string;
+}
+
+const SolidBtn = (props: Props) => {
+	const {
+		className,
+		onClick,
+		title,
+		titleClassName,
+		LeftIcon,
+		leftIconSize,
+		leftIconClassName,
+		RightIcon,
+		rightIconSize,
+		rightIconClassName,
+		...rest
+	} = props;
+
+	return (
+		<button
+			className={twMerge(
+				"w-full bg-primary dark:bg-dark-primary hover:bg-primary/90 dark:hover:bg-dark-primary/90 flex items-center justify-center px-4 py-2.5 font-muli text-primary-foreground dark:text-dark-primary-foreground rounded-md",
+				className,
+			)}
+			aria-label={`${title} button`}
+			onClick={onClick}
+			{...rest}
+		>
+			{LeftIcon && (
+				<LeftIcon
+					size={leftIconSize ?? 22}
+					className={twMerge(
+						"text-primary-foreground dark:text-dark-primary-foreground mr-1",
+						leftIconClassName,
+					)}
+				/>
+			)}
+
+			<p
+				className={twMerge(
+					"text-primary-foreground dark:text-dark-primary-foreground text-base",
+					titleClassName,
+				)}
+			>
+				{title}
+			</p>
+
+			{RightIcon && (
+				<RightIcon
+					size={rightIconSize ?? 22}
+					className={twMerge(
+						"text-primary-foreground dark:text-dark-primary-foreground mr-1",
+						rightIconClassName,
+					)}
+				/>
+			)}
+		</button>
+	);
+};
+
+export default SolidBtn;
