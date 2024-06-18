b332ca45e763532545fcd9c847ebb87a9f755dcd
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-06-19 01:27:51 +0530

    feat: :sparkles: add embed functions

--- store/message-configs-store.ts
+++ store/message-configs-store.ts
@@ -1,10 +1,13 @@
-import { MessageConfig } from "@/types/models";
+import { Embed, MessageConfig } from "@/types/models";
 import { create } from "zustand";
 
 interface MessageConfigStore {
 	messageConfigs: Map<string, MessageConfig>;
 	setConfig: (key: string, config: MessageConfig) => void;
 	deleteConfigs: (keys: Array<string>) => void;
+	appendEmbed: (key: string, embed: Embed) => void;
+	updateEmbed: (key: string, index: number, embed: Embed) => void;
+	deleteEmbed: (key: string, index: number) => void;
 }
 
 export const useMessageConfigStore = create<MessageConfigStore>()((set) => ({
@@ -30,6 +33,78 @@ export const useMessageConfigStore = create<MessageConfigStore>()((set) => ({
 			for (const key of keys) {
 				newConfigs.delete(key);
 			}
+
+			return {
+				messageConfigs: newConfigs,
+			};
+		}),
+	appendEmbed: (key, embed) =>
+		set((prev) => {
+			const newConfigs: Map<string, MessageConfig> = new Map(
+				prev.messageConfigs,
+			);
+
+			const oldMessageConfig = newConfigs.get(key);
+
+			if (!oldMessageConfig)
+				return {
+					messageConfigs: newConfigs,
+				};
+
+			newConfigs.set(key, {
+				...oldMessageConfig,
+				embeds: [embed, ...oldMessageConfig.embeds],
+			});
+
+			return {
+				messageConfigs: newConfigs,
+			};
+		}),
+	updateEmbed: (key, index, embed) =>
+		set((prev) => {
+			const newConfigs: Map<string, MessageConfig> = new Map(
+				prev.messageConfigs,
+			);
+
+			const oldMessageConfig = newConfigs.get(key);
+
+			if (!oldMessageConfig)
+				return {
+					messageConfigs: newConfigs,
+				};
+
+			oldMessageConfig.embeds[index] = embed;
+
+			newConfigs.set(key, {
+				...oldMessageConfig,
+				embeds: [...oldMessageConfig.embeds],
+			});
+
+			return {
+				messageConfigs: newConfigs,
+			};
+		}),
+	deleteEmbed: (key, index) =>
+		set((prev) => {
+			const newConfigs: Map<string, MessageConfig> = new Map(
+				prev.messageConfigs,
+			);
+
+			const oldMessageConfig = newConfigs.get(key);
+
+			if (!oldMessageConfig)
+				return {
+					messageConfigs: newConfigs,
+				};
+
+			const remainingEmbeds = oldMessageConfig.embeds.filter(
+				(_, i) => i !== index,
+			);
+
+			newConfigs.set(key, {
+				...oldMessageConfig,
+				embeds: remainingEmbeds,
+			});
 
 			return {
 				messageConfigs: newConfigs,
